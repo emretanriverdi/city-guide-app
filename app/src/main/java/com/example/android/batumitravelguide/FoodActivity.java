69346e4997de76dff30d3aba49e8ba91e35aeec9
Emre Tanrıverdi <tanriverdi.emre2@gmail.com>
2018-08-17 16:35:31 +0300

    Finished MainActivity and its intents.

---
+++ app/src/main/java/com/example/android/batumitravelguide/FoodActivity.java
@@ -0,0 +1,7 @@
+package com.example.android.batumitravelguide;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class FoodActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/android/batumitravelguide/MainActivity.java
+++ app/src/main/java/com/example/android/batumitravelguide/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.batumitravelguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView generalInfo = findViewById(R.id.text_view_general_info);
+        generalInfo.setOnClickListener(sendTo(InfoActivity.class));
+
+        TextView localDishes = findViewById(R.id.text_view_local_dishes);
+        localDishes.setOnClickListener(sendTo(FoodActivity.class));
+
+        TextView historicalPlaces = findViewById(R.id.text_view_historical_places);
+        historicalPlaces.setOnClickListener(sendTo(PlacesActivity.class));
+
+        TextView usefulLinks = findViewById(R.id.text_view_useful_links);
+        usefulLinks.setOnClickListener(sendTo(LinksActivity.class));
+    }
+
+    private View.OnClickListener sendTo(final Class className) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, className));
+            }
+        };
     }
 }
